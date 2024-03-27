1fcb7fdbbf9ba8fc2fb18aba87568b89b381e8ff
codescientists <piyushpp2412@gmail.com>
2024-03-27 16:16:53 +0530

    Added auth images for auth pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return <div className="flex h-screen">
   <div className="hidden lg:flex items-center justify-center flex-1 bg-white text-black">
     <div className="max-w-md text-center">
-      {/* <img src="/assets/register.jpg" alt=""/>  */}
+      <img src="/assets/auth.jpg" alt=""/> 
     </div>
   </div>
   <div className="w-full bg-gray-100 lg:w-1/2 flex items-center justify-center">
